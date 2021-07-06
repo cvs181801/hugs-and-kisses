11f124c609cc0cad0ede7e48af38658e90589227
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-06 13:57:08 -0700

    test basic filter method

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+function checkHugsAndKisses(letters) {
+   letters.toLowerCase();
+   const xArray = letters.filter(letter, x);
+   const oArray = letters.filter(letter, o);  
+}
+
+console.log(checkHugsAndKisses([x,x,x,o,o,o]));
