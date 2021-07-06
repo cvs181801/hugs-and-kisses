8ebf6f06e4eee5e4e61fa8439d429752edfa98e4
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-06 14:13:09 -0700

    try with a parameter

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 array1 = [x, x, x, o, o, o];
 
-function checkKisses() {
-   return array1 = x;
+function checkKisses(letter) {
+   return array1.letter = x;
 }
 
-console.log(array1.filter(checkKisses));
+console.log(array1.filter(checkKisses(letter)));
