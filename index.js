4cd4000313fe553c27b0a78f02e95c662d851c47
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-06 14:03:55 -0700

    remove the parameter

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-array1 = [x,x,x,o,o,o];
+array1 = [x, x, x, o, o, o];
 
-function checkKisses(letters) {
-   return letters === x;
+function checkKisses() {
+   return x;
 }
 
 console.log(array1.filter(checkKisses));
