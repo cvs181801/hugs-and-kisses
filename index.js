651062a6298f58ad86511d86e021736e8d32ac3a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-08 16:05:43 -0700

    add basic math.random

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
+
+
+//create a way to randomly generate x's and o's 
+
+function randomXandO() {
+   return Math.floor(Math.random())
+}
+
+console.log(randomXandO);
 array1 = [x, x, x, o, o, o];
 
 function checkKisses(letter) {
    return letter = x;
 }
 
-console.log(array1.filter(checkKisses(letter)));
+//console.log(array1.filter(checkKisses(letter)));
