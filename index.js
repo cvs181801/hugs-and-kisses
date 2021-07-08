78597dab06e384affcfb585347bfd433fda7ae92
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-08 16:06:41 -0700

    try again

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function randomXandO() {
    return Math.floor(Math.random())
 }
 
-console.log(randomXandO);
-array1 = [x, x, x, o, o, o];
+console.log(randomXandO());
+// array1 = [x, x, x, o, o, o];
 
-function checkKisses(letter) {
-   return letter = x;
-}
+// function checkKisses(letter) {
+//    return letter = x;
+// }
 
 //console.log(array1.filter(checkKisses(letter)));
