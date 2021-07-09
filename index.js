1ab633e98982584f78375de4076e51f467ec97f3
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-08 17:41:43 -0700

    try to add in the event listener

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ if (exes === ohs) {
 generateButton.addEventListener('click', function(e) {
    e.preventDefault();
    console.log("it worked!");
-   return randomXandO();
+   return randomXandO(), countXsandOs();
 })
 
 
